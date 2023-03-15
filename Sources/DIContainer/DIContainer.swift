7f85f3ec3d7f6187072f34616a3ca769164ca840
Majd Koshakji <koshakji@gmail.com>
2023-03-16 00:00:00 +0100

    Added optional name parameter to all registration and resolving methods

--- Sources/DIContainer/DIContainer.swift
+++ Sources/DIContainer/DIContainer.swift
@@ -12,29 +12,41 @@ public class DIContainer: DIContainerProtocol {
         self.dependencies = [:]
     }
     
-    private func `key`<Protocol>(for type: Protocol.Type) -> String {
-        return "\(type)"
+    private func `key`<Protocol>(for type: Protocol.Type, named name: String?) -> String {
+        if let name {
+            return "\(name)|\(type)"
+        } else {
+            return "\(type)"
+        }
     }
     
-    public func register<Protocol>(type: Protocol.Type, value: DILifecycle<Protocol>) {
-        self.dependencies[self.key(for: type)] = value
+    public func register<Protocol>(
+        type: Protocol.Type,
+        named name: String? = nil,
+        value: DILifecycle<Protocol>
+    ) {
+        self.dependencies[self.key(for: type, named: name)] = value
     }
     
-    public func has<Protocol>(type: Protocol.Type = Protocol.self) -> Bool {
-        return self.dependencies.index(forKey: self.key(for: type)) != nil
+    public func has<Protocol>(
+        type: Protocol.Type = Protocol.self,
+        named name: String? = nil
+    ) -> Bool {
+        return self.dependencies.index(forKey: self.key(for: type, named: name)) != nil
     }
     
-    public subscript<Protocol>(_ type: Protocol.Type = Protocol.self) -> Protocol? {
-        get {
-            guard let lifecycle = self.dependencies[self.key(for: type)] as? DILifecycle<Protocol> else {
-                return nil
-            }
-            
-            switch lifecycle {
-            case .eagerSingleton(let value): return value
-            case .lazySingleton(let lazy): return lazy.value(for: self)
-            case .transient(let factory): return factory(self)
-            }
+    public func resolve<Protocol>(
+        _ type: Protocol.Type = Protocol.self,
+        named name: String? = nil
+    ) -> Protocol? {
+        guard let lifecycle = self.dependencies[self.key(for: type, named: name)] as? DILifecycle<Protocol> else {
+            return nil
+        }
+        
+        switch lifecycle {
+        case .eagerSingleton(let value): return value
+        case .lazySingleton(let lazy): return lazy.value(for: self)
+        case .transient(let factory): return factory(self)
         }
     }
 }
