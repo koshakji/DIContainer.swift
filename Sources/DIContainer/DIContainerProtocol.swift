eaaf1558bfd77bedf208d421f443c1a387b11388
Majd Koshakji <koshakji@gmail.com>
2023-03-16 00:00:00 +0100

    Added optional name parameter to all registration and resolving methods

--- Sources/DIContainer/DIContainerProtocol.swift
+++ Sources/DIContainer/DIContainerProtocol.swift
@@ -8,29 +8,58 @@
 public typealias DIFactory<T> = (any DIContainerProtocol) -> T?
 
 public protocol DIContainerProtocol {
-    func register<Protocol>(type: Protocol.Type, value: DILifecycle<Protocol>)
-    func has<Protocol>(type: Protocol.Type) -> Bool
-    subscript<Protocol>(_: Protocol.Type) -> Protocol? { get }
+    func register<Protocol>(type: Protocol.Type, named: String?, value: DILifecycle<Protocol>)
+    func has<Protocol>(type: Protocol.Type, named: String?) -> Bool
+    func resolve<Protocol>(_ type: Protocol.Type, named: String?) -> Protocol?
 }
 
 extension DIContainerProtocol {
-    public func register<Protocol>(type: Protocol.Type = Protocol.self, transient factory: @escaping DIFactory<Protocol>) {
-        self.register(type: type, value: .transient(factory))
+    public func register<Protocol>(
+        type: Protocol.Type = Protocol.self,
+        named name: String? = nil,
+        transient factory: @escaping DIFactory<Protocol>) {
+        self.register(
+            type: type,
+            named: name,
+            value: .transient(factory)
+        )
     }
     
-    public func register<Protocol>(type: Protocol.Type = Protocol.self, lazySingleton factory: @escaping DIFactory<Protocol>) {
-        self.register(type: type, value: .lazySingleton(DILazyContainer(factory: factory)))
+    public func register<Protocol>(
+        type: Protocol.Type = Protocol.self,
+        named name: String? = nil,
+        lazySingleton factory: @escaping DIFactory<Protocol>
+    ) {
+        self.register(
+            type: type,
+            named: name,
+            value: .lazySingleton(DILazyContainer(factory: factory))
+        )
     }
     
-    public func register<Protocol>(type: Protocol.Type = Protocol.self, eagerSingleton dependency: Protocol) {
-        self.register(type: type, value: .eagerSingleton(dependency))
+    public func register<Protocol>(
+        type: Protocol.Type = Protocol.self,
+        named name: String? = nil,
+        eagerSingleton dependency: Protocol
+    ) {
+        self.register(
+            type: type,
+            named: name,
+            value: .eagerSingleton(dependency)
+        )
     }
     
-    public func resolve<Protocol>(_ type: Protocol.Type = Protocol.self) -> Protocol? {
-        return self[type]
+    public subscript<Protocol>(
+        _ type: Protocol.Type,
+        named name: String? = nil
+    ) -> Protocol? {
+        get { self.resolve(type, named: name) }
     }
     
-    public func forceResolve<Protocol>(_ type: Protocol.Type = Protocol.self) -> Protocol {
-        return self.resolve()!
+    public func forceResolve<Protocol>(
+        _ type: Protocol.Type = Protocol.self,
+        named name: String? = nil
+    ) -> Protocol {
+        return self.resolve(type, named: name)!
     }
 }
